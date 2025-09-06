b1038f2ac452e54a93aac2b69951398ab9a41108
bassamadnan <bassamadnan2012@gmail.com>
2025-09-06 09:03:20 +0530

    alias name

--- sql/pgexporter_ext--0.1.0.sql
+++ sql/pgexporter_ext--0.1.0.sql
@@ -1,19 +1,19 @@
 CREATE FUNCTION pgexporter_used_space(IN dir text) RETURNS bigint
-AS 'MODULE_PATHNAME'
+AS 'MODULE_PATHNAME', 'pgexporter_ext_used_space'
 LANGUAGE C STRICT;
 
 REVOKE ALL ON FUNCTION pgexporter_used_space FROM PUBLIC;
 GRANT EXECUTE ON FUNCTION pgexporter_used_space TO pg_monitor;
 
 CREATE FUNCTION pgexporter_free_space(IN dir text) RETURNS bigint
-AS 'MODULE_PATHNAME'
+AS 'MODULE_PATHNAME', 'pgexporter_ext_free_space'
 LANGUAGE C STRICT;
 
 REVOKE ALL ON FUNCTION pgexporter_free_space FROM PUBLIC;
 GRANT EXECUTE ON FUNCTION pgexporter_free_space TO pg_monitor;
 
 CREATE FUNCTION pgexporter_total_space(IN dir text) RETURNS bigint
-AS 'MODULE_PATHNAME'
+AS 'MODULE_PATHNAME', 'pgexporter_ext_total_space'
 LANGUAGE C STRICT;
 
 REVOKE ALL ON FUNCTION pgexporter_total_space FROM PUBLIC;
